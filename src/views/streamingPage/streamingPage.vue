813d6e760c4eeafb9ffcf0c4eb87aaf113a4d21e
Enchograph <Ender0213@outlook.com>
2025-06-23 17:09:28 +0800

    直播进行页面

--- src/views/streamingPage/streamingPage.vue
+++ src/views/streamingPage/streamingPage.vue
@@ -1,3 +1,16 @@
-<template src="./streamingPage.html"></template>
-<script src="./streamingPage.js"></script>
-<style src="./streamingPage.css" scoped></style>
+<template>
+    <div class="page-container">
+
+        <LiveStreamingAssistant />
+    </div>
+</template>
+
+<script>
+import LiveStreamingAssistant from '../../components/LiveStream.vue';
+
+export default {
+    components: {
+        LiveStreamingAssistant
+    }
+}
+</script>
